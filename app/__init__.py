ea77712e30758bd542980593904ebed0522a9a2e
cleopasrotich <cleopasrotich81@gmail.com>
2018-09-26 17:03:40 +0300

    modified file

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask
-from flask_api import FlaskAPI
-from flask_sqlalchemy import SQLAlchemy
-from instance.config import app_config  # a local import
+# from flask_api import FlaskAPI
+# from flask_sqlalchemy import SQLAlchemy
+# from instance.config import app_config  # a local import
 
-db = SQLAlchemy()
+#db = SQLAlchemy()
 
 
 def create_app(config_name):
-    app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
+    # app = FlaskAPI(__name__, instance_relative_config=True)
+    # app.config.from_object(app_config[config_name])
+    # app.config.from_pyfile('config.py')
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # db.init_app(app)
 
     app = Flask(__name__)
     return app
