2a37bec6464177d4a4e74774b1abd41215c0a8f7
cleopasrotich <cleopasrotich81@gmail.com>
2018-09-26 16:56:14 +0300

    modified tests

--- test/test_orders.py
+++ test/test_orders.py
@@ -5,20 +5,11 @@ app = Flask(__name__)
 
 
 class Testorders(unittest.TestCase):
-    def setUp(self):
-        self.client = self.app.test_client
-        with self.app.app_context():
-
-            db.create_all()
 
     def test_all(self):
         result = app.test_client()
         response = result.get('/fast_foods/api/v1/orders',content_type='application/json')
         self.assertTrue(response.status_code, 200)
-        res = self.client().get('/fast_foods/api/v1/orders')
-        self.assertEqual(res.status_code, 201)
-
-
 
     def test_all_orders(self):
         result = app.test_client()
